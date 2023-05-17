2d0024a33a39b2f1e12d00a37b020d39b8926911
s110145 <131227796+s110145@users.noreply.github.com>
2023-05-17 11:46:04 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,6 @@
+for (let index = 0; index <= 2; index++) {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    basic.showNumber(3 - index)
+}
+music.playTone(262, music.beat(BeatFraction.Whole))
+basic.showString("GO!")
